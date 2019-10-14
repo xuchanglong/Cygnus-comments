bb7a3436842dbd38e551c99680abc0cf952b06a4
xuchanglong <wwwitworld@sina.com>
2019-10-14 13:40:29 +0800

    增加注释代码。

--- g++/stl_pair.h
+++ g++/stl_pair.h
@@ -24,6 +24,10 @@
  * purpose.  It is provided "as is" without express or implied warranty.
  */
 
+/**
+ * 这是一个内部头文件，供其他 STL 头文件使用，
+ * 正常情况下，程序员不应该使用该文件。
+*/
 /* NOTE: This is an internal header file, included by other STL headers.
  *   You should not attempt to use it directly.
  */
@@ -33,34 +37,48 @@
 
 __STL_BEGIN_NAMESPACE
 
+/**
+ * 仅仅是包含了两个变量的结构体，作为 map 的键-值对。
+ * 结构体中默认情况下变量和函数均是 public 。
+*/
 template <class T1, class T2>
-struct pair {
+struct pair
+{
   typedef T1 first_type;
   typedef T2 second_type;
 
   T1 first;
   T2 second;
+  /**
+   * T1 和 T2 若是对象，则赋值临时对象。
+   * 若是基本数据类型，则赋值为 0 。
+  */
   pair() : first(T1()), second(T2()) {}
-  pair(const T1& a, const T2& b) : first(a), second(b) {}
+  pair(const T1 &a, const T2 &b) : first(a), second(b) {}
 
 #ifdef __STL_MEMBER_TEMPLATES
   template <class U1, class U2>
-  pair(const pair<U1, U2>& p) : first(p.first), second(p.second) {}
+  pair(const pair<U1, U2> &p) : first(p.first), second(p.second)
+  {
+  }
 #endif
 };
 
 template <class T1, class T2>
-inline bool operator==(const pair<T1, T2>& x, const pair<T1, T2>& y) { 
-  return x.first == y.first && x.second == y.second; 
+inline bool operator==(const pair<T1, T2> &x, const pair<T1, T2> &y)
+{
+  return x.first == y.first && x.second == y.second;
 }
 
 template <class T1, class T2>
-inline bool operator<(const pair<T1, T2>& x, const pair<T1, T2>& y) { 
-  return x.first < y.first || (!(y.first < x.first) && x.second < y.second); 
+inline bool operator<(const pair<T1, T2> &x, const pair<T1, T2> &y)
+{
+  return x.first < y.first || (!(y.first < x.first) && x.second < y.second);
 }
 
 template <class T1, class T2>
-inline pair<T1, T2> make_pair(const T1& x, const T2& y) {
+inline pair<T1, T2> make_pair(const T1 &x, const T2 &y)
+{
   return pair<T1, T2>(x, y);
 }
 
