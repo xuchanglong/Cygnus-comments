76b38cd8b75bc6b5bea7edeff95c156bd79a1bf7
xuchanglong <wwwitworld@sina.com>
2020-02-10 22:05:28 +0800

    优化代码格式。

--- g++/stl_set.h
+++ g++/stl_set.h
@@ -42,7 +42,8 @@ template <class Key, class Compare = less<Key>, class Alloc = alloc>
 #else
 template <class Key, class Compare, class Alloc = alloc>
 #endif
-class set {
+class set
+{
 public:
   // typedefs:
 
@@ -50,10 +51,12 @@ public:
   typedef Key value_type;
   typedef Compare key_compare;
   typedef Compare value_compare;
+
 private:
-  typedef rb_tree<key_type, value_type, 
-                  identity<value_type>, key_compare, Alloc> rep_type;
-  rep_type t;  // red-black tree representing set
+  typedef rb_tree<key_type, value_type,
+                  identity<value_type>, key_compare, Alloc>
+      rep_type;
+  rep_type t; // red-black tree representing set
 public:
   typedef typename rep_type::const_pointer pointer;
   typedef typename rep_type::const_pointer const_pointer;
@@ -69,31 +72,40 @@ public:
   // allocation/deallocation
 
   set() : t(Compare()) {}
-  explicit set(const Compare& comp) : t(comp) {}
+  explicit set(const Compare &comp) : t(comp) {}
 
 #ifdef __STL_MEMBER_TEMPLATES
   template <class InputIterator>
   set(InputIterator first, InputIterator last)
-    : t(Compare()) { t.insert_unique(first, last); }
+      : t(Compare())
+  {
+    t.insert_unique(first, last);
+  }
 
   template <class InputIterator>
-  set(InputIterator first, InputIterator last, const Compare& comp)
-    : t(comp) { t.insert_unique(first, last); }
+  set(InputIterator first, InputIterator last, const Compare &comp)
+      : t(comp) { t.insert_unique(first, last); }
 #else
-  set(const value_type* first, const value_type* last) 
-    : t(Compare()) { t.insert_unique(first, last); }
-  set(const value_type* first, const value_type* last, const Compare& comp)
-    : t(comp) { t.insert_unique(first, last); }
+  set(const value_type *first, const value_type *last)
+      : t(Compare())
+  {
+    t.insert_unique(first, last);
+  }
+  set(const value_type *first, const value_type *last, const Compare &comp)
+      : t(comp) { t.insert_unique(first, last); }
 
   set(const_iterator first, const_iterator last)
-    : t(Compare()) { t.insert_unique(first, last); }
-  set(const_iterator first, const_iterator last, const Compare& comp)
-    : t(comp) { t.insert_unique(first, last); }
+      : t(Compare()) { t.insert_unique(first, last); }
+  set(const_iterator first, const_iterator last, const Compare &comp)
+      : t(comp) { t.insert_unique(first, last); }
 #endif /* __STL_MEMBER_TEMPLATES */
 
-  set(const set<Key, Compare, Alloc>& x) : t(x.t) {}
-  set<Key, Compare, Alloc>& operator=(const set<Key, Compare, Alloc>& x) { 
-    t = x.t; 
+  set(const set<Key, Compare, Alloc> &x) : t(x.t)
+  {
+  }
+  set<Key, Compare, Alloc> &operator=(const set<Key, Compare, Alloc> &x)
+  {
+    t = x.t;
     return *this;
   }
 
@@ -103,83 +115,97 @@ public:
   value_compare value_comp() const { return t.key_comp(); }
   iterator begin() const { return t.begin(); }
   iterator end() const { return t.end(); }
-  reverse_iterator rbegin() const { return t.rbegin(); } 
+  reverse_iterator rbegin() const { return t.rbegin(); }
   reverse_iterator rend() const { return t.rend(); }
   bool empty() const { return t.empty(); }
   size_type size() const { return t.size(); }
   size_type max_size() const { return t.max_size(); }
-  void swap(set<Key, Compare, Alloc>& x) { t.swap(x.t); }
+  void swap(set<Key, Compare, Alloc> &x) { t.swap(x.t); }
 
   // insert/erase
-  typedef  pair<iterator, bool> pair_iterator_bool; 
-  pair<iterator,bool> insert(const value_type& x) { 
-    pair<typename rep_type::iterator, bool> p = t.insert_unique(x); 
+  typedef pair<iterator, bool> pair_iterator_bool;
+  pair<iterator, bool> insert(const value_type &x)
+  {
+    pair<typename rep_type::iterator, bool> p = t.insert_unique(x);
     return pair<iterator, bool>(p.first, p.second);
   }
-  iterator insert(iterator position, const value_type& x) {
+  iterator insert(iterator position, const value_type &x)
+  {
     typedef typename rep_type::iterator rep_iterator;
-    return t.insert_unique((rep_iterator&)position, x);
+    return t.insert_unique((rep_iterator &)position, x);
   }
 #ifdef __STL_MEMBER_TEMPLATES
   template <class InputIterator>
-  void insert(InputIterator first, InputIterator last) {
+  void insert(InputIterator first, InputIterator last)
+  {
     t.insert_unique(first, last);
   }
 #else
-  void insert(const_iterator first, const_iterator last) {
+  void insert(const_iterator first, const_iterator last)
+  {
     t.insert_unique(first, last);
   }
-  void insert(const value_type* first, const value_type* last) {
+  void insert(const value_type *first, const value_type *last)
+  {
     t.insert_unique(first, last);
   }
 #endif /* __STL_MEMBER_TEMPLATES */
-  void erase(iterator position) { 
+  void erase(iterator position)
+  {
     typedef typename rep_type::iterator rep_iterator;
-    t.erase((rep_iterator&)position); 
+    t.erase((rep_iterator &)position);
   }
-  size_type erase(const key_type& x) { 
-    return t.erase(x); 
+  size_type erase(const key_type &x)
+  {
+    return t.erase(x);
   }
-  void erase(iterator first, iterator last) { 
+  void erase(iterator first, iterator last)
+  {
     typedef typename rep_type::iterator rep_iterator;
-    t.erase((rep_iterator&)first, (rep_iterator&)last); 
+    t.erase((rep_iterator &)first, (rep_iterator &)last);
   }
   void clear() { t.clear(); }
 
   // set operations:
 
-  iterator find(const key_type& x) const { return t.find(x); }
-  size_type count(const key_type& x) const { return t.count(x); }
-  iterator lower_bound(const key_type& x) const {
+  iterator find(const key_type &x) const { return t.find(x); }
+  size_type count(const key_type &x) const { return t.count(x); }
+  iterator lower_bound(const key_type &x) const
+  {
     return t.lower_bound(x);
   }
-  iterator upper_bound(const key_type& x) const {
-    return t.upper_bound(x); 
+  iterator upper_bound(const key_type &x) const
+  {
+    return t.upper_bound(x);
   }
-  pair<iterator,iterator> equal_range(const key_type& x) const {
+  pair<iterator, iterator> equal_range(const key_type &x) const
+  {
     return t.equal_range(x);
   }
-  friend bool operator== __STL_NULL_TMPL_ARGS (const set&, const set&);
-  friend bool operator< __STL_NULL_TMPL_ARGS (const set&, const set&);
+  friend bool operator== __STL_NULL_TMPL_ARGS(const set &, const set &);
+  friend bool operator<__STL_NULL_TMPL_ARGS(const set &, const set &);
 };
 
 template <class Key, class Compare, class Alloc>
-inline bool operator==(const set<Key, Compare, Alloc>& x, 
-                       const set<Key, Compare, Alloc>& y) {
+inline bool operator==(const set<Key, Compare, Alloc> &x,
+                       const set<Key, Compare, Alloc> &y)
+{
   return x.t == y.t;
 }
 
 template <class Key, class Compare, class Alloc>
-inline bool operator<(const set<Key, Compare, Alloc>& x, 
-                      const set<Key, Compare, Alloc>& y) {
+inline bool operator<(const set<Key, Compare, Alloc> &x,
+                      const set<Key, Compare, Alloc> &y)
+{
   return x.t < y.t;
 }
 
 #ifdef __STL_FUNCTION_TMPL_PARTIAL_ORDER
 
 template <class Key, class Compare, class Alloc>
-inline void swap(set<Key, Compare, Alloc>& x, 
-                 set<Key, Compare, Alloc>& y) {
+inline void swap(set<Key, Compare, Alloc> &x,
+                 set<Key, Compare, Alloc> &y)
+{
   x.swap(y);
 }
 
